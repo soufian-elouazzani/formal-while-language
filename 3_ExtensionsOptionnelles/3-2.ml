c60aaff70b8935c06b5bf636012da945042e6eaf
soufian <soufianelouazzani939@gmail.com>
2025-12-07 15:48:21 +0100

    adding the question 3.2

--- 3_ExtensionsOptionnelles/3-2.ml
+++ 3_ExtensionsOptionnelles/3-2.ml
@@ -1,137 +1,136 @@
-#use "anacomb.ml"
 
-(* ---------------------- TOKENS ---------------------- *)
+
+(* Exercice 3.2 *)
+
+(* Définition des tokens *)
+
 
 type token =
-  | Tident of string
-  | Tnombre of int
-  | Tplus
-  | Tmoins
-  | Tfois
-  | Tdiv
-  | Tegal
-  | TparO
-  | TparF
-  | Tpointvirgule
-  | Tsi
-  | Talors
-  | Tsinon
-  | Tfait
-  | Ttantque
-  | Tfaire
+  (* Identifiers and numbers *)
+  | Ident of string
+  | Number of int
 
-(* ---------------------- AST ---------------------- *)
+  (* Boolean constants *)
+  | TRUE
+  | FALSE
 
-type expr =
-  | EConst of int
-  | EVar of string
-  | EPlus of expr * expr
-  | EMoins of expr * expr
-  | ETimes of expr * expr
-  | EDiv of expr * expr
+  (* Arithmetic operators *)
+  | PLUS       (* + *)
+  | MINUS      (* - *)
+  | TIMES      (* * *)
+  | DIV        (* / *)
 
-type instr =
-  | Affect of string * expr
-  | Suite of instr list
-  | Si of expr * instr * instr
-  | Tantque of expr * instr
+  (* Boolean operators *)
+  | AND        (* && *)
+  | OR         (* || *)
+  | NOT        (* ! *)
 
-(* ---------------------------------------------------------- *)
-(* TERMINAUX : conversion token -> AST                        *)
-(* ---------------------------------------------------------- *)
+  (* Comparison operators *)
+  | EQ         (* == *)
+  | LT         (* < *)
+  | LE         (* <= *)
+  | GT         (* > *)
+  | GE         (* >= *)
 
-let t_ident =
-  terminal_res (function Tident s -> Some s | _ -> None)
+  (* Assignment and punctuation *)
+  | ASSIGN     (* := *)
+  | LPAREN     (* ( *)
+  | RPAREN     (* ) *)
+  | LBRACE     (* { *)
+  | RBRACE     (* } *)
+  | SEMI       (* ; *)
 
-let t_nombre =
-  terminal_res (function Tnombre n -> Some (EConst n) | _ -> None)
+  | EOF
 
-let t_symbole tok =
-  terminal tok
 
-(* ---------------------------------------------------------- *)
-(* ANALYSEUR D'EXPRESSIONS  (VERSION COMPACTE)                *)
-(* ---------------------------------------------------------- *)
 
-(* forward declarations *)
-let rec a_expr l = expr_add l
-and expr_add l =
-  ((expr_mul ++> fun e1 ->
-     ((t_symbole Tplus -+> expr_add) ++> fun e2 ->
-        epsilon_res (EPlus(e1,e2)))
-     +|
-     ((t_symbole Tmoins -+> expr_add) ++> fun e2 ->
-        epsilon_res (EMoins(e1,e2)))
-   )
-   +|
-   expr_mul) l
+(* Analyseur lexical *)
 
-and expr_mul l =
-  ((expr_atom ++> fun e1 ->
-     ((t_symbole Tfois -+> expr_mul) ++> fun e2 ->
-        epsilon_res (ETimes(e1,e2)))
-     +|
-     ((t_symbole Tdiv -+> expr_mul) ++> fun e2 ->
-        epsilon_res (EDiv(e1,e2)))
-   )
-   +|
-   expr_atom) l
+let is_letter c =
+  ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c = '_'
 
-and expr_atom l =
-  ( t_nombre
-    +|
-    ( t_ident ++> fun name -> epsilon_res (EVar name) )
-    +|
-    ( t_symbole TparO -+> a_expr ++> fun e ->
-        t_symbole TparF -+> epsilon_res e )
-  ) l
+let is_digit c =
+  '0' <= c && c <= '9'
 
-(* ---------------------------------------------------------- *)
-(* INSTRUCTIONS                                                *)
-(* ---------------------------------------------------------- *)
+let rec skip_spaces = function
+  | c::q when c = ' ' || c = '\n' || c = '\t' -> skip_spaces q
+  | s -> s
 
-let rec a_instr l =
-  (
-    (* affectation : ident = expr ; *)
-    (t_ident ++> fun name ->
-       t_symbole Tegal -+> a_expr ++> fun e ->
-         t_symbole Tpointvirgule -+> epsilon_res (Affect (name, e))
-    )
+let rec read_number acc = function
+  | c::q when is_digit c ->
+      let digit = int_of_char c - int_of_char '0' in
+      read_number (acc * 10 + digit) q
+  | s -> (acc, s)
 
-    +|
+let rec read_ident buf = function
+  | c::q when is_letter c || is_digit c ->
+      read_ident (buf ^ String.make 1 c) q
+  | s -> (buf, s)
 
-    (* if expr then instr else instr *)
-    (t_symbole Tsi -+> a_expr ++> fun e ->
-       t_symbole Talors -+> a_instr ++> fun i1 ->
-         t_symbole Tsinon -+> a_instr ++> fun i2 ->
-           epsilon_res (Si(e,i1,i2))
-    )
+(* Lexer principal *)
 
-    +|
+let rec lex = function
+  | [] -> (EOF, [])
 
-    (* while expr do instr *)
-    (t_symbole Ttantque -+> a_expr ++> fun e ->
-       t_symbole Tfaire -+> a_instr ++> fun i ->
-         epsilon_res (Tantque(e,i))
-    )
+  | s ->
+      let s = skip_spaces s in
+      match s with
+      | [] -> (EOF, [])
 
-    +|
+      (* Identifiers or keywords *)
+      | c::q when is_letter c ->
+          let (id, rest) = read_ident (String.make 1 c) q in
+          begin match id with
+          | "true"  -> (TRUE, rest)
+          | "false" -> (FALSE, rest)
+          | _       -> (Ident id, rest)
+          end
 
-    (* bloc : instr instr ... fait *)
-    (t_symbole Tfait -+> epsilon_res (Suite []))
-  ) l
+      (* Numbers *)
+      | c::q when is_digit c ->
+          let digit = int_of_char c - int_of_char '0' in
+          let (n, rest) = read_number digit q in
+          (Number n, rest)
 
-(* Séquence : suite d'instructions jusqu'à 'fait' *)
-and a_suite l =
-  (
-    (a_instr ++> fun i ->
-       a_suite ++> fun (Suite rest) ->
-         epsilon_res (Suite (i :: rest))
-    )
-    +|
-    (t_symbole Tfait -+> epsilon_res (Suite []))
-  ) l
+      (* Two-character operators *)
+      | ':'::'='::rest -> (ASSIGN, rest)
+      | '='::'='::rest -> (EQ, rest)
+      | '<'::'='::rest -> (LE, rest)
+      | '>'::'='::rest -> (GE, rest)
+      | '&'::'&'::rest -> (AND, rest)
+      | '|'::'|'::rest -> (OR, rest)
 
-(* programme = suite d’instructions *)
-let a_programme = a_suite
+      (* One-character tokens *)
+      | '+'::rest -> (PLUS, rest)
+      | '-'::rest -> (MINUS, rest)
+      | '*'::rest -> (TIMES, rest)
+      | '/'::rest -> (DIV, rest)
+      | '!'::rest -> (NOT, rest)
+      | '<'::rest -> (LT, rest)
+      | '>'::rest -> (GT, rest)
+      | '('::rest -> (LPAREN, rest)
+      | ')'::rest -> (RPAREN, rest)
+      | '{'::rest -> (LBRACE, rest)
+      | '}'::rest -> (RBRACE, rest)
+      | ';'::rest -> (SEMI, rest)
 
+      | c::_ -> failwith (Printf.sprintf "Unknown char: %c" c)
+
+
+(* tokenisation complet *)
+
+let rec tokenize chars =
+  match lex chars with
+  | (EOF, _) -> [EOF]
+  | (tok, rest) -> tok :: tokenize rest
+
+let tokenize_string s =
+  tokenize (List.init (String.length s) (String.get s))
+
+(* Chainer lexicale → syntaxe *)
+
+let parse_from_string s =
+  let toks = tokenize_string s in
+  match parser_program toks with
+  | Some (ast, []) -> ast
+  | _ -> failwith "Syntax error"
